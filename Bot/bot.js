a98c33482256c31a80c41de564c7bea982c58449
teya-teya <142177100+teya-teya@users.noreply.github.com>
2023-08-31 12:44:38 +0300

    Умеет вбивать фразу по буквам

--- Bot/bot.js
+++ Bot/bot.js
@@ -13,13 +13,26 @@ let btnS = document.querySelectorAll(".search")[0];
 let bingInput = document.getElementsByName("q")[0];
 let keywords = ["Алый вечер над синей рекою",
                 "Секреты облаков",
-                "Август - не лето"]
+                "Август - не лето",
+                "Загадочные рассветы",
+                "Дожди вернулись... ",
+                "Пёс Сумкин"]
 let keyword = keywords[getRandom(0, keywords.length)];
 
+//Работаю на главной странице
 if (btnS != undefined) {
-  bingInput.value = keyword;
-btnS.click();
-} else {
+  let i = 0;
+  let timerId = setInterval(function() {
+    bingInput.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+    btnS.click();
+  }
+}, 700)
+
+  } else {
+// Работаю на странице поисковой выдачи
 for (let i = 0; i < links.length; i++) {
     if(links[i].href.indexOf("stihi.ru") != -1) {
         console.log("Нашел строку " + links[i]);
