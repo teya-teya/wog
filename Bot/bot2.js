d4eba2de487740e28a9a7d597831e4ffaff6371f
teya-teya <142177100+teya-teya@users.noreply.github.com>
2023-09-01 08:29:15 +0300

    Работает на целевом сайте, случайно уходит в поисковик, (ошибка - переход на сторонние ресурсы!!)

--- Bot/bot2.js
+++ Bot/bot2.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Bot of Yandex
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Bot of Yandex
 // @author       Panferova Anastasiya
 // @match        https://ya.ru/*
 // @match        https://yandex.ru/*
+// @match        https://*.stihi.ru/*
 // @grant        none
 // ==/UserScript==
 
@@ -36,11 +37,25 @@ if (btnY != undefined) {
       btnY.click();
     }
   }, 700)
+  } else if (location.hostname == "stihi.ru" || location.hostname == "o.stihi.ru" || location.hostname == "shop.stihi.ru") {
+    console.log("Мы на целовом сайте");
+    setInterval(() => {
+      let index = getRandom(0, links.length);
+      if (getRandom(0, 101) >= 75) {
+      location.href = "https://ya.ru/";
+          }
+      if (links.length == 0) {
+        location.href = "https://stihi.ru/";
+      }
+      if (links[index].href.indexOf("stihi.ru", "o.stihi.ru", "shop.stihi.ru")) {
+        links[index].click()
+      }
+    }, getRandom(3000, 5000))
   } else {
     // Работаем на странице поисковой выдачи
     let nextYaPage = true;
     for (let i = 0; i < links.length; i++) {
-      if(links[i].href.indexOf("stihi.ru") != -1) {
+      if(links[i].href.indexOf("stihi.ru", "o.stihi.ru", "shop.stihi.ru") != -1) {
         console.log("Нашел строку " + links[i]);
         let link = links[i];
         nextYaPage = false;
@@ -48,6 +63,18 @@ if (btnY != undefined) {
         break;
       }
     }
+    let elementExist = setInterval(() => {
+      let elem = document.querySelector(".Pager-Item_current");
+      if (elem != null) {
+        if (elem.innerText == "5") {
+          nextYaPage = false;
+          location.href = "https://ya.ru/";
+        }
+        clearInterval(elementExist);
+      }
+    }, 100)
+    
+    // Если не нашли на первой странице выдачи, то листаем дальше
     if (nextYaPage) {
       let nextPage = document.querySelector(".VanillaReact.Pager-Item.Pager-Item_type_next");
       setTimeout(() => {nextPage.click();}, getRandom(3500, 5500));
